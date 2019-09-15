29b94c8eb12f2c2223753434496816a9c0e3d783
Ryan Zhou <ryanyz10@gmail.com>
2019-09-15 06:02:09 -0400

    Add instabase processing, not tested :(

--- grocr-server/app.js
+++ grocr-server/app.js
@@ -7,7 +7,18 @@ const port = 8000;
 const time = (new Date()).getTime();
 const seed = uuidv3(`${time}`,'1b671a64-40d5-491e-99b0-da01ff1f3341');
 const users = new Map();
-//const users = [];
+
+// HAHA we are such good programmers :P
+const FS_URL_BASE = "https://www.instabase.com/api/v1/drives";
+const FLOW_URL_BASE = "https://www.instabase.com/api/v1";
+
+const OCR_INPUT_DIR = "ryanyz10/my-repo/fs/Instabase Drive/ocr-text-extraction/input";
+const EXTRACT_INPUT_DIR = "ryanyz10/my-repo/fs/Instabase Drive/information-extraction/input";
+
+const OCR_URL = "ryanyz10/my-repo/fs/Instabase Drive/ocr-text-extraction/OCRFlow.ibflow";
+const EXTRACT_URL = "ryanyz10/my-repo/fs/Instabase Drive/information-extraction/InformationExtractionFlow.ibflow";
+
+const INSTABASE_TOKEN = "6DuAoNlviiYLTY42nBX5NP9pH0oS2v";
 
 app.use(bodyParser.json({
     limit: '50mb',
@@ -34,8 +45,8 @@ app.post('/createUser', function(req,res){
         user.email = req.body.email;
         user.name = req.body.name;
         user.password = req.body.password;
-        user.products = [];
-        user.pictures = [];
+        user.receipts = [];
+        user.inventory = {};
         users.set(tokenId, user);
         res.json(tokenId);
         res.end();
@@ -68,20 +79,97 @@ app.post('/login', function(req,res) {
 });
 
 app.post('/postImage', function(req,res){
-    let id = `${users.get(req.token).username}-${new Date().getTime()}`;
+    // let id = `${users.get(req.token).username}-${new Date().getTime()}`;
     let data = {...req.body};
-    base64Img.img("data:image/jpeg;base64,"+data.img, 'img/', `img-${id}`, function(err, filepath) {
-        if(!err) {
-            users.get(req.token).pictures.push(id);
-        } else {
-            res.sendStatus(400);
+    let jsonResponse = await fetch(`${FS_URL_BASE}/${EXTRACT_INPUT_DIR}/input.jpg`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${INSTABASE_TOKEN}`, 
+            'Instabase-API-Args': JSON.stringify({
+                'type': 'file',
+                'cursor': 0,
+                'if_exists': 'overwrite',
+                'mime_type': 'image/jpeg' 
+            })
+        },
+        body: `data:image/png;base64,${data.img}`
+    });
+
+    if (jsonResponse.data.status === 'ERROR') {
+        res.sendStatus(jsonResponse.data.statusCode);
+    }
+
+    jsonResponse = await fetch(`${FLOW_URL_BASE}/flow/run_flow_async`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${INSTABASE_TOKEN}`
+        },
+        body: JSON.stringify({
+            'input_dir': EXTRACT_INPUT_DIR,
+            'ibflow_path': EXTRACT_URL,
+            'output_has_run_id': false,
+            'delete_out_dir': true
+        })
+    });
+
+    if (jsonResponse.data.status === 'ERROR') {
+        res.sendStatus(400);
+    }
+
+    const jobId = jsonResponse.data.data.job_id;
+    const outputDir = jsonResponse.data.data.output_folder;
+    jsonResponse = await fetch(`${FLOW_URL_BASE}/jobs/status?job_id=${jobId}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${INSTABASE_TOKEN}`
         }
     });
-    res.sendStatus(200);
+
+    while (jsonResponse.data.state !== 'DONE') {
+        jsonResponse = await fetch(`${FLOW_URL_BASE}/jobs/status?job_id=${jobId}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${INSTABASE_TOKEN}`
+            }
+        });
+    }
+
+    jsonResponse = await fetch(`${FLOW_URL_BASE}/flow/export/review_batch`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${INSTABASE_TOKEN}`
+        },
+        body: JSON.stringify({
+            "path": outputDir
+        })
+    });
+
+    if (jsonResponse.data.status === 'ERROR') {
+        res.sendStatus(400);
+    }
+
+    const receiptData = jsonResponse.data.csv_text;
+    console.log(receiptData);
 });
 
-app.get('/getData', function(req,res){
-    res.sendStatus(200);
+app.get('/getInventory', function(req,res){
+    res.json(users.get(req.token).inventory);
+    res.end();
+});
+
+app.get('/getReceipts', function(req,res){
+    res.json(users.get(req.token).receipts);
+    res.end();
 });
 
 app.use('/img/',express.static(__dirname + '/img/'));
